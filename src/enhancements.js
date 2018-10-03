b94e41fd91234402afdd015218e73bd5f3edae14
Dan Burzo <danburzo@gmail.com>
2018-10-03 17:33:54 +0300

    v0.1.7

--- src/enhancements.js
+++ src/enhancements.js
@@ -9,11 +9,11 @@ function imagesAtFullSize(doc) {
 			<img src='original-size.png'/>
 	 */
 	Array.from(doc.querySelectorAll('a > img:only-child')).forEach(img => {
+		let anchor = img.parentNode;
 		let original = anchor.href;
 
 		// only replace if the HREF matches an image file
 		if (original.match(/\.(png|jpg|jpeg|gif|svg)$/)) {
-			let anchor = img.parentNode;
 			img.setAttribute('src', original);
 			anchor.parentNode.replaceChild(img, anchor);
 		}
