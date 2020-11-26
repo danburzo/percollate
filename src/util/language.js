0ea68c86188fe5f27e7c36f16858bad2b3167b43
Y K <yasarkkk@protonmail.com>
2020-11-26 23:15:25 +0100

    Improved performance of language extraction

--- src/util/language.js
+++ src/util/language.js
@@ -1,15 +1,12 @@
 const franc = require('franc-all');
-const iso6393 = require('iso-639-3');
+const iso6393 = require('iso-639-3/to-1.json');
 
 function textToLang(text) {
 	const franLanguage = franc(text);
-	let lang = 'en';
-	iso6393.forEach(language => {
-		if (language.iso6393 === franLanguage) {
-			lang = language.iso6391;
-		}
-	});
-	return lang;
+	if (iso6393.hasOwnProperty(franLanguage)) {
+		return iso6393[franLanguage];
+	}
+	return 'en';
 }
 
 module.exports = { textToLang };
