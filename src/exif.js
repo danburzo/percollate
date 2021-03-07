4c61f7c03051ae50ab42b0ce04b67d659c0be8be
Dan Burzo <dan@danburzo.ro>
2021-03-07 19:44:32 +0200

    Only set pieces of PDF metadata when they are provided; fixes #120

--- src/exif.js
+++ src/exif.js
@@ -9,7 +9,11 @@ module.exports = async function addExif(existingPdfBytes, metaData) {
 	const pdfDoc = await PDFDocument.load(existingPdfBytes, {
 		updateMetadata: false
 	});
-	pdfDoc.setTitle(metaData.Title);
-	pdfDoc.setAuthor(metaData.Author);
+	if (metaData.Title) {
+		pdfDoc.setTitle(metaData.Title);
+	}
+	if (metaData.Author) {
+		pdfDoc.setAuthor(metaData.Author);
+	}
 	return pdfDoc.save();
 };
