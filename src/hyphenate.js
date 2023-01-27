8da96471ccc00f1d057ca5a79aa7201774236796
Dan Burzo <dan@danburzo.ro>
2023-01-27 15:07:25 +0200

    (chore) update some dependencies

--- src/hyphenate.js
+++ src/hyphenate.js
@@ -1,4 +1,7 @@
 import Hyphenator from 'hyphenopoly';
+import { dirname } from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 
 const DEFAULT_LANG = 'en-us';
 
@@ -8,6 +11,14 @@ function getHypenatorByLang(lang) {
 		: DEFAULT_LANG;
 	return Hyphenator.config({
 		sync: true,
+		loaderSync: file => {
+			return readFileSync(
+				new URL(
+					`../node_modules/hyphenopoly/patterns/${file}`,
+					import.meta.url
+				)
+			);
+		},
 		require: [language],
 		defaultLanguage: DEFAULT_LANG,
 		minWordLength: 6,
