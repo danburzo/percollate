1d1e54e50b02170cff3cc483d84a263b39dc8da3
Adam Vongrej <vongrejadam@gmail.com>
2023-03-03 22:39:25 +0100

    Fixed issue with converting ArrayBuffer to base64 (#154)
    * Fixed issue with converting ArrayBuffer to base64
    
    * Added missing node:buffer import
    
    ---------
    
    Co-authored-by: Adam Vongrej <vongrad@Adams-MacBook-Pro-2.local>

--- src/util/fetch-base64.js
+++ src/util/fetch-base64.js
@@ -1,7 +1,8 @@
 import fetch from 'node-fetch';
+import { Buffer } from 'node:buffer';
 
 export default function fetchBase64(url, fetchOptions = {}) {
 	return fetch(url, fetchOptions)
 		.then(r => r.arrayBuffer())
-		.then(buff => buff.toString('base64'));
+		.then(buff => Buffer.from(buff).toString('base64'));
 }
